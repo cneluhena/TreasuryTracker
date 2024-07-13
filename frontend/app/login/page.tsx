38383c0ffc9bc6a9a7a9bcf02cffaaba0e9842d5
cneluhena <129386035+cneluhena@users.noreply.github.com>
2024-07-13 15:12:51 +0530

    started to build signup page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -9,15 +9,28 @@ import {
   Paper,
   Snackbar,
   TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { ChangeEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+import { cookies } from "next/headers";
+import { request } from "https";
+import { getCookie, setCookie } from "cookies-next";
+import { get } from "http";
 
-const LoginForm = () => {
+
+const SignupForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  
   const [open, setOpen] = useState(false);
   const router = useRouter();
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
 
   const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
@@ -26,15 +39,17 @@ const LoginForm = () => {
   const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
+ 
 
   const handleSignIn = async () => {
     try {
       const response = await fetch("http://localhost:5000/user/login", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json"
+         
         },
-        body: JSON.stringify({ username: username, password: password }),
+        body: JSON.stringify({ username: username, password: password })
       });
 
       if (!response.ok) {
@@ -47,8 +62,9 @@ const LoginForm = () => {
       if (!token) {
         throw new Error("Login failed!");
       }
-
       localStorage.setItem("token", token);
+      
+
       router.push("/home");
     } catch (error: any) {
       console.error("Error logging in:", error.message);
@@ -68,8 +84,8 @@ const LoginForm = () => {
         elevation={3}
         sx={{
           p: 4,
-          height: "60vh",
-          width: "350px",
+          height: isSmallScreen ? "320px" : "350px",
+          width: "380px",
           m: "auto",
           display: "flex",
           flexDirection: "column",
@@ -77,27 +93,47 @@ const LoginForm = () => {
           alignItems: "center",
         }}
       >
+        <Typography variant="h5" component="div" gutterBottom  sx={{ 
+            fontFamily: 'Arial, sans-serif', // Change to your desired font family
+            fontSize: '24px', // Change to your desired font size
+            fontWeight: 'bold', // Change to your desired font weight
+          }}>
+          Login
+        </Typography>
         <FormControl>
+        <Grid container spacing={3} justifyContent={'center'}>
+
+        <Grid item xs={12} sm={12}>
           <TextField
             label="Username"
             size="small"
             fullWidth
-            sx={{ mb: 2 }}
+            value={username}
             type="text"
             onChange={handleUsernameChange}
           />
+          </Grid>
+
+            <Grid item xs={12}>          
           <TextField
             size="small"
             label="Password"
             type="password"
             fullWidth
-            sx={{ mb: 2 }}
+            value={password}
             onChange={handlePasswordChange}
           />
-        </FormControl>
-        <Button variant="contained" fullWidth onClick={handleSignIn}>
-          Sign-in
+          </Grid>
+         
+            <Grid item xs={12} sm={12}>
+            <Button variant="contained" fullWidth onClick={handleSignIn}>
+          Login
         </Button>
+        
+          </Grid>
+          </Grid>
+        </FormControl>
+     
       </Paper>
       <Snackbar anchorOrigin = {{ vertical: 'bottom', horizontal: 'center' }} onClose={()=>{setOpen(false)}}open = {open} autoHideDuration={2000} >
         <Alert  variant="filled"  severity="error">
@@ -109,4 +145,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignupForm;
---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,325 @@
+"use client";
+import { FormControl } from "@mui/base/FormControl";
+import {
+  Alert,
+  Box,
+  Button,
+  Grid,
+  OutlinedInput,
+  Paper,
+  Snackbar,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { cookies } from "next/headers";
+import { request } from "https";
+import { getCookie, setCookie } from "cookies-next";
+import { get } from "http";
+
+
+const SignupForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const [firstError, setFirstError] = useState(false);
+  const [lastError, setLastError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [userError, setUserError] = useState(false);
+  const [passError, setPassError] = useState(false);
+
+  const [firstErrorText, setFirstErrorText] = useState("");
+  const [lastErrorText, setlastErrorText] = useState("");
+  const [emailErrorText, setEmailErrorText] = useState("");
+  const [phoneErrorText, setPhoneErrorText] = useState("");
+  const [userErrorText, setUserErrorText] = useState("");
+  const [passErrorText, setPassErrorText] = useState("");
+
+  const [hasError, setHasError] = useState(false);
+
+  const router = useRouter();
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+   if (!event.target.validity.valueMissing){
+    setUserError(false);
+    setUsername(event.target.value);
+   }
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length < 8) {
+        setPassError(true);
+        setPassErrorText("Password must at least 8 characters long");
+      } else{
+          setPassError(false);
+      }
+    setPassword(event.target.value);
+  };
+
+  const handleFirstNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.validity.valueMissing){
+        setFirstError(false);
+        setFirstName(event.target.value);
+    }
+  };
+
+  const handleLastNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.validity.valueMissing){
+        setLastError(false);
+        setLastName(event.target.value);
+    }
+  };
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.validity.valueMissing){
+        setEmailError(false);
+        setEmail(event.target.value);
+    }
+  };
+
+  const handlePhoneChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const isNumber = /^\d*$/.test(value);  // Regular expression to check if the input contains only numbers
+    setPhoneNumber(value);
+    if (!(isNumber && value.length == 10)){
+        setPhoneError(true);
+        setPhoneErrorText("Enter a valid phone number");
+    } else{
+        setPhoneError(false);
+    }
+
+       
+  };
+  
+
+  const validate = () =>{
+ 
+    if (firstName === ""){
+        setFirstErrorText("First Name is required")
+        setFirstError(true);
+        setHasError(true);
+    }
+    else{
+        setFirstError(false);
+    }
+
+    if (lastName === ""){
+        setlastErrorText("Last Name is required")
+        setLastError(true);
+        setHasError(true);
+
+    }else{
+        setLastError(false);
+    }
+
+    if(email === ""){
+        setEmailErrorText("Email is required");
+        setEmailError(true);
+        setHasError(true);
+
+    } else{
+        setEmailError(false);
+    } 
+
+
+    if (phoneNumber === ""){
+        setPhoneError(true);
+        setPhoneErrorText("Phone Number is required");
+        setHasError(true);
+        
+    }
+
+   if (username === ""){
+    setUserErrorText("Username is required");
+    setUserError(true);
+    setHasError(true);
+
+    } else{
+        setUserError(false);
+    }
+
+    if (password === ""){
+        setPassErrorText("Password is required");
+        setPassError(true);
+        setHasError(true);
+    
+    } 
+    return hasError;
+
+}
+
+  const handleSignUp = async () => {
+    validate();
+    if (hasError){
+        return;
+    }
+   else{
+
+    try {
+
+      const response = await fetch("http://localhost:5000/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+         
+        },
+        body: JSON.stringify({ username: username, password: password, firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber })
+      });
+
+      if (!response.ok) {
+        
+        throw new Error("Error in registration");
+      }
+
+
+    } catch (error: any) {
+      console.error("Error logging in:", error.message);
+      setOpen(true);
+   
+    }
+
+
+  }};
+
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: "100vh" }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          p: 4,
+          minheight: isSmallScreen ? "550px" : "550px",
+          width: "400px",
+          m: "auto",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        
+        }}
+      >
+        <Typography variant="h5" component="div" gutterBottom  sx={{ 
+            fontFamily: 'Arial, sans-serif', // Change to your desired font family
+            fontSize: '24px', // Change to your desired font size
+            fontWeight: 'bold', // Change to your desired font weight
+          }}>
+          Signup
+        </Typography>
+        <FormControl>
+        <Grid container justifyContent='center' spacing={3}>
+        <Grid item xs={12} sm={12}>
+        <TextField
+            label="First Name"
+            size="small"
+            fullWidth
+            error={firstError}
+            helperText={firstError ? firstErrorText : ""}
+            value={firstName}
+            type="text"
+            onChange={handleFirstNameChange}
+          />
+        </Grid>
+
+        <Grid item xs = {12} sm={12}>
+          <TextField
+            label="Last Name"
+            size="small"
+            fullWidth
+            error = {lastError}
+            helperText = {lastError ? lastErrorText : ""}
+            value={lastName}
+            type="text"
+            onChange={handleLastNameChange}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Email"
+            size="small"
+            fullWidth
+            error = {emailError}
+            helperText = {emailError ? emailErrorText : ""}
+            value-={email}
+            type="text"
+            onChange={handleEmailChange}
+          />
+        </Grid>
+
+        <Grid item xs = {12} sm={12}>
+          <TextField
+            label="Phone Number"
+            size="small"
+            fullWidth
+            error={phoneError}
+            helperText={phoneError ? phoneErrorText : ""}
+            value={phoneNumber}
+            type="text"
+            onChange={handlePhoneChange}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Username"
+            size="small"
+            fullWidth
+            error={userError}
+            helperText={userError ? userErrorText : ""}
+            value={username}
+            type="text"
+            onChange={handleUsernameChange}
+          />
+          </Grid>
+
+            <Grid item xs={12}>          
+          <TextField
+            size="small"
+            label="Password"
+            type="password"
+            fullWidth
+            error={passError}
+            helperText={passError ? passErrorText : ""}
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          </Grid>
+   
+            <Grid item xs={12} sm={12}>
+            <Button variant="contained" fullWidth onClick={handleSignUp}>
+          Sign-up
+        </Button>
+          
+          </Grid>
+          </Grid>
+        </FormControl>
+     
+      </Paper>
+    
+      <Snackbar anchorOrigin = {{ vertical: 'bottom', horizontal: 'center' }} onClose={()=>{setHasError(false)}} open = {hasError} autoHideDuration={2000} >
+        <Alert  variant="filled"  severity="error">
+          Fill all the fields
+        </Alert>
+      </Snackbar>
+    </Grid>
+    
+  );
+};
+
+export default SignupForm;

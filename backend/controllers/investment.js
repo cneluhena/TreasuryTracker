24021f5936e1907a4fbc8ac4f365b31b80247ea7
cneluhena <129386035+cneluhena@users.noreply.github.com>
2024-09-22 07:07:44 +0530

    added some routes in backedn to fetch details for homepage

--- backend/controllers/investment.js
+++ backend/controllers/investment.js
@@ -1,8 +1,8 @@
 const Investment = require("../models/investment");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { response } = require("express");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+
 
 //adding investment to the tracker
 const addInvestment = async (req, res, next) => {
@@ -58,12 +58,70 @@ const getInvestments = async (req, res, next) => {
     if (!findAllInvestments) {
       return res.status(404).send("No investments for this user");
     }
-
+    console.log(findAllInvestments);
     return res.status(200).json(findAllInvestments);
   } catch (error) {
     return res.status(200).send("Internal Error");
   }
 };
+
+const getTotalInvestments = async (req, res, next)=>{
+
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(404).send("No user Found");
+    }
+
+    const userId = user._id.toString(); //converting the userId to string
+    const amount = await Investment.aggregate([
+      {
+        $match:{userId: userId}
+      },
+      {
+        $group:{
+          _id: null,
+          amount: {$sum:"$investmentAmount"}
+
+        }
+      }
+    ])
+    return res.status(200).json(amount[0].amount);
+
+  } catch (error) {
+    return res.status(200).send("Internal Error");
+  }
+
+}
+
+const getTotalActiveInvestments = async (req, res, next)=>{
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(404).send("No user Found");
+    }
+
+    const userId = user._id.toString(); //converting the userId to string
+    const activeInvestments = await Investment.aggregate([
+      {
+        $match:  {userId:userId, maturityDate:{$gt: new Date()}}
+      },
+      {
+        $group:{
+          _id: null,
+          amount: {$sum:"$investmentAmount"}
+
+        }
+      }
+     ]
+    )
+    return res.status(200).json(activeInvestments[0].amount)
+}
+      catch (error) {
+        return res.status(200).send("Internal Error");
+}
+}
+
 
 // const getFuturePredictions = async (req, res, next) => {
 //   try {
@@ -73,4 +131,4 @@ const getInvestments = async (req, res, next) => {
 //     return res.status(400).send("Error in getting future predictions");
 //   }
 // };
-module.exports = { addInvestment, getInvestments };
+module.exports = { addInvestment, getInvestments, getTotalInvestments, getTotalActiveInvestments };

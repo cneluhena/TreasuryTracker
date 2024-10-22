f34da4fef85c75d077612cd173a81aadd588ceb2
Niroshan2001 <2001niroshan123@gmail.com>
2024-10-22 10:27:24 +0530

    change API endpoint URL in interests.js and myapi.py files

--- backend/controllers/interests.js
+++ backend/controllers/interests.js
@@ -70,7 +70,7 @@ const predict = async (req, res) => {
         .json({ error: "Please provide a period for prediction" });
     }
 
-    const response = await axios.post("http://127.0.0.1:5000/predict", {
+    const response = await axios.post("http://127.0.0.1:8080/predict", {
       period,
     });
     const predictions = response.data;

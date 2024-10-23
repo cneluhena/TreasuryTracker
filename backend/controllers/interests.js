fa3594c4c000657c9f51ca81ecb6fedbeb723608
Maneth Ninduwara <maneth.21@cse.mrt.ac.lk>
2024-10-23 14:21:01 +0530

    fixing url of model in interests

--- backend/controllers/interests.js
+++ backend/controllers/interests.js
@@ -109,7 +109,7 @@ const predict = async (req, res) => {
         .json({ error: "Please provide a period for prediction" });
     }
 
-    const response = await axios.post("http://127.0.0.1:8080/predict", {
+    const response = await axios.post(process.env.NEXT_PUBLIC_MODEL_URL+"/predict", {
       period,
     });
     const predictions = response.data;
